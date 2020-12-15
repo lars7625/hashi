a29bae730c4510ab84823eabd5ae60d94f70a5ea
Lars <larskemperman@gmail.com>
2020-12-15 17:52:49 +0100

    tweaks

--- src/components/Puzzle.vue
+++ src/components/Puzzle.vue
@@ -5,35 +5,69 @@
         <!-- invisible lines to trigger function -->
         <g :stroke="gridColor" stroke-width="1">
           <!-- horizontal grid line -->
-          <line v-for="(n, i) in numOfRows" :key="'hl' + i" :x1="0" :y1="i * scale" :x2="gridWidth" :y2="i * scale"></line>
+          <line
+          v-for="(n, i) in numOfRows" :key="'hl' + i" :x1="0" :y1="i *
+          scale" :x2="gridWidth" :y2="i * scale">
+          </line>
           <!-- vertica l grid line -->
-          <line v-for="(n, i) in numOfCols" :key="'vl' + i" :x1="i * scale" :y1="0" :x2="i * scale" :y2="gridHeight"></line>
+          <line
+          v-for="(n, i) in numOfCols" :key="'vl' + i" :x1="i * scale"
+          :y1="0" :x2="i * scale" :y2="gridHeight">
+          </line>
         </g>
         <g :stroke="bridgeColor" :stroke-width="bridgeStrokeW">
         <!-- if one bridge -->
-        <line v-for="(n,i) in singleBridgeList" :key="'sBridge' + i" :x1="xGridPosN(singleBridgeList[i][0])" :y1="yGridPosN(singleBridgeList[i][0])" :x2="xGridPosN(singleBridgeList[i][1])" :y2="yGridPosN(singleBridgeList[i][1])"></line>
+        <line
+        v-for="(n,i) in singleBridgeList" :key="'sBridge' + i"
+        :x1="xGridPosN(singleBridgeList[i][0])" :y1="yGridPosN(singleBridgeList[i][0])"
+        :x2="xGridPosN(singleBridgeList[i][1])" :y2="yGridPosN(singleBridgeList[i][1])">
+        </line>
         <!-- if two bridges -->
-        <line v-for="(n,i) in doubleBridgeList" :key="'dBridgeTop' + i" :x1="xGridPosB1of2(doubleBridgeList[i][0])" :y1="yGridPosB1of2(doubleBridgeList[i][0])" :x2="xGridPosB1of2(doubleBridgeList[i][1])" :y2="yGridPosB1of2(doubleBridgeList[i][1])"></line>
-        <line v-for="(n,i) in doubleBridgeList" :key="'dBridgeBot' + i" :x1="xGridPosB2of2(doubleBridgeList[i][0])" :y1="yGridPosB2of2(doubleBridgeList[i][0])" :x2="xGridPosB2of2(doubleBridgeList[i][1])" :y2="yGridPosB2of2(doubleBridgeList[i][1])"></line>
+        <line
+        v-for="(n,i) in doubleBridgeList" :key="'dBridgeTop' + i"
+        :x1="xGridPosB1of2(doubleBridgeList[i][0])" :y1="yGridPosB1of2(doubleBridgeList[i][0])"
+        :x2="xGridPosB1of2(doubleBridgeList[i][1])" :y2="yGridPosB1of2(doubleBridgeList[i][1])">
+        </line>
+        <line
+        v-for="(n,i) in doubleBridgeList" :key="'dBridgeBot' + i"
+        :x1="xGridPosB2of2(doubleBridgeList[i][0])" :y1="yGridPosB2of2(doubleBridgeList[i][0])"
+        :x2="xGridPosB2of2(doubleBridgeList[i][1])" :y2="yGridPosB2of2(doubleBridgeList[i][1])">
+        </line>
         </g>
-        <!-- highlist bridge on hover -->
-        <line stroke="red" stroke-opacity="70%" stroke-width="5" :key="'bridgehover'" :x1="xGridPosN(highlightBridgeList[0])" :y1="yGridPosN(highlightBridgeList[0])" :x2="xGridPosN(highlightBridgeList[1])" :y2="yGridPosN(highlightBridgeList[1])"></line>
         <!-- hidden bridges that serve to set the bridges -->
-        <line  v-for="(n, i) in connNtN" @mouseover="highLightBridge(connNtN[i])" @mouseleave="unHighLightBridge()" v-on:click="setBridges(i)" stroke="white" stroke-opacity="0%" stroke-width="5" :key="'bridgeClick' + i" :x1="xGridPosN(connNtN[i][0])" :y1="yGridPosN(connNtN[i][0])" :x2="xGridPosN(connNtN[i][1])" :y2="yGridPosN(connNtN[i][1])"></line>
+        <line
+        v-for="(n, i) in connNtN" @mouseover="highLightBridge(connNtN[i])"
+        stroke="white" stroke-opacity="0%" stroke-width="5" :key="'bridgeClick' + i"
+        :x1="xGridPosN(connNtN[i][0])" :y1="yGridPosN(connNtN[i][0])"
+        :x2="xGridPosN(connNtN[i][1])" :y2="yGridPosN(connNtN[i][1])">
+        </line>
+        <!-- highlist bridge on hover -->
+        <line
+        stroke="red"  stroke-opacity="70%" stroke-width="10" :key="'bridgehover'"
+        @mouseleave="unHighLightBridge()" v-on:click="setBridges(highlightBridgeList)"
+        :x1="xGridPosN(highlightBridgeList[0])" :y1="yGridPosN(highlightBridgeList[0])"
+        :x2="xGridPosN(highlightBridgeList[1])" :y2="yGridPosN(highlightBridgeList[1])">
+        </line>
         <!-- circles for nodes -->
         <g fill="white" :stroke="circleColor" stroke-width="2">
-          <circle v-for="(n, i) in numOfNodes" :key="i" :cx="calcXPosC(i)" :cy="calcYPosC(i)" :r="circleRadius" />
+          <circle
+          v-for="(n, i) in numOfNodes" :key="i" :cx="calcXPosC(i)"
+          :cy="calcYPosC(i)" :r="circleRadius" />
         </g>
         <!-- text numbers in hashi circles -->
-        <text v-for="(n, i) in numOfNodes" :key="i" :x="calcXPosT(i)" :y="calcYPosT(i)" fill="black" dominant-baseline="middle" text-anchor="middle">{{ board[i].connections }}</text>
+        <text
+        v-for="(n, i) in numOfNodes" :key="i" :x="calcXPosT(i)" :y="calcYPosT(i)"
+        fill="black" dominant-baseline="middle" text-anchor="middle">
+        {{ boardNodes[i].connections }}
+        </text>
       </svg>
     </div>
     <div>
-      <button v-on:click="checkSolution()">Check  Puzzle</button>
+      <button v-on:click="checkSolution()">Check  Puzzle5</button>
     </div>
   </div>
 </template>
-
+connNtN
 <script>
 import boardGenerator from '@/components/board.js'
 
@@ -57,6 +91,7 @@ export default {
       singleBridgeList: [],
       doubleBridgeList: [],
       highlightBridgeList: [0, 0],
+      unClickableNodes: [],
       bridgeColor: 'black',
       circleColor: 'grey',
       gridColor: 'grey',
@@ -78,19 +113,36 @@ export default {
       return this.offSetX + ' ' + this.offSetY + ' ' + gridWidth + ' ' + gridHeight
     },
     numOfNodes: function () {
-      return this.board.length
+      return this.boardNodes.length
+    },
+    boardNodes: function () {
+      return boardGenerator(this.numOfCols, this.numOfRows)
     },
     board: function () {
-      return boardGenerator(this.numOfCols, this.numOfRows)
+      let board = []
+      const nodePositions = []
+      this.boardNodes.forEach(el => {
+        nodePositions.push(el.position)
+      })
+      board = (new Array(this.numOfCols * this.numOfRows)).fill(0)
+      board.map((boardEl, boardIdx) => {
+        if (nodePositions.includes(boardIdx)) {
+          board[boardIdx] = 1
+        }
+      })
+      return board
     },
     connNtN: function () {
       const nodesConn = []
       const parentNodes = []
-      this.board.forEach(node => {
+      let nodes = []
+      this.boardNodes.forEach(node => {
         node.nodeToNode.map(el => {
           // check for duplicates if not add pair
           if (!parentNodes.includes(el)) {
-            nodesConn.push([node.position, el])
+            nodes = [node.position, el]
+            nodes.sort((a, b) => a - b)
+            nodesConn.push([nodes[0], nodes[1]])
             parentNodes.push(node.position)
           }
         })
@@ -100,16 +152,16 @@ export default {
   },
   methods: {
     calcXPosC: function (i) {
-      return (this.board[i].position % this.numOfCols) * this.scale
+      return (this.boardNodes[i].position % this.numOfCols) * this.scale
     },
     calcYPosC: function (i) {
-      return Math.floor(this.board[i].position / this.numOfCols) * this.scale
+      return Math.floor(this.boardNodes[i].position / this.numOfCols) * this.scale
     },
     calcXPosT: function (i) {
-      return (this.board[i].position % this.numOfCols) * this.scale
+      return (this.boardNodes[i].position % this.numOfCols) * this.scale
     },
     calcYPosT: function (i) {
-      return Math.floor(this.board[i].position / this.numOfCols) * this.scale
+      return Math.floor(this.boardNodes[i].position / this.numOfCols) * this.scale
     },
     xGridPosN: function (nodeLoc) {
       return (nodeLoc % this.numOfCols) * this.scale
@@ -129,18 +181,29 @@ export default {
     yGridPosB2of2: function (nodeLoc) {
       return (Math.floor(nodeLoc / this.numOfCols)) * this.scale + this.doubleBridgeOffSet
     },
-    setBridges: function (i) {
-      if (!this.singleBridgeList.includes(this.connNtN[i]) && !this.doubleBridgeList.includes(this.connNtN[i])) {
-        this.singleBridgeList.push(this.connNtN[i])
-      } else if (this.singleBridgeList.includes(this.connNtN[i])) {
-        this.singleBridgeList = this.arrRemove(this.singleBridgeList, this.connNtN[i])
-        this.doubleBridgeList.push(this.connNtN[i])
-      } else if (this.doubleBridgeList.includes(this.connNtN[i])) {
-        this.doubleBridgeList = this.arrRemove(this.doubleBridgeList, this.connNtN[i])
+    getGridPos: function (nodeLoc) {
+      const gridPos = { row: 0, col: 0 }
+      gridPos.row = (Math.floor(nodeLoc / this.numOfCols))
+      gridPos.col = nodeLoc % this.numOfCols
+
+      return gridPos
+    },
+    setBridges: function (nodes) {
+      if (!this.singleBridgeList.includes(nodes) && !this.doubleBridgeList.includes(nodes)) {
+        this.singleBridgeList.push(nodes)
+        this.setPointsNodes([nodes[0], nodes[1]], 'set')
+      } else if (this.singleBridgeList.includes(nodes)) {
+        this.singleBridgeList = this.arrRemove(this.singleBridgeList, nodes)
+        this.doubleBridgeList.push(nodes)
+      } else if (this.doubleBridgeList.includes(nodes)) {
+        this.doubleBridgeList = this.arrRemove(this.doubleBridgeList, nodes)
+        this.setPointsNodes([nodes[0], nodes[1]], 'remove')
       }
     },
     highLightBridge: function (nodes) {
-      this.highlightBridgeList = nodes
+      if (!this.unClickableNodes.includes(nodes)) {
+        this.highlightBridgeList = nodes
+      }
     },
     unHighLightBridge: function () {
       this.highlightBridgeList = [0, 0]
@@ -151,7 +214,7 @@ export default {
       })
     },
     checkSolution: function () {
-      const nodeList = [...this.board]
+      const nodeList = [...this.boardNodes]
       const oneBridge = this.singleBridgeList.flat()
       const twoBridges = this.doubleBridgeList.flat()
       nodeList.map(el => {
@@ -178,10 +241,67 @@ export default {
       } else {
         alert('you suck')
       }
+    },
+    setPointsNodes: function (nodePair, action) {
+      const nodes = [nodePair[0], nodePair[1]]
+      nodes.sort((a, b) => a - b)
+      const node1GridPos = this.getGridPos(nodes[0])
+      const node2GridPos = this.getGridPos(nodes[1])
+      let newVal = 0
+      if (action === 'set') newVal = -1
+      // same row
+      if (node1GridPos.row === node2GridPos.row) {
+        for (let i = nodes[0] + 1; i < nodes[1]; i++) {
+          this.board[i] = newVal
+        }
+      } else if (node1GridPos.col === node2GridPos.col) {
+        for (let i = nodes[0] + this.numOfCols; i < nodes[1]; i += this.numOfCols) {
+          this.board[i] = newVal
+        }
+      }
+      this.setUnClickableNodes()
+    },
+    setUnClickableNodes: function () {
+      let betweenNodesCounter = 0
+      this.connNtN.forEach(nodes => {
+        const node1Grid = this.getGridPos(nodes[0])
+        const node2Grid = this.getGridPos(nodes[1])
+        // check west and south - nodes are sorted ascending
+        if (node1Grid.row === node2Grid.row) {
+          for (let i = nodes[0] + 1; i < nodes[1]; i++) {
+            if (this.board[i] === -1 && !this.singleBridgeList.includes(nodes) && !this.doubleBridgeList.includes(nodes) && !this.unClickableNodes.includes(nodes)) {
+              this.unClickableNodes.push(nodes)
+              break
+            } else if (this.board[i] === 0 && this.unClickableNodes.includes(nodes)) {
+              betweenNodesCounter++
+              console.log(betweenNodesCounter)
+              console.log(node2Grid.col - node1Grid.col)
+              if (betweenNodesCounter === (node2Grid.col - node1Grid.col - 1)) {
+                this.unClickableNodes = this.arrRemove(this.unClickableNodes, nodes)
+                betweenNodesCounter = 0
+              }
+            }
+          }
+        } else if (node1Grid.col === node2Grid.col) {
+          for (let i = nodes[0] + this.numOfCols; i < nodes[1]; i += this.numOfCols) {
+            if (this.board[i] === -1 && !this.singleBridgeList.includes(nodes) && !this.doubleBridgeList.includes(nodes) && !this.unClickableNodes.includes(nodes)) {
+              this.unClickableNodes.push(nodes)
+              break
+            } else if (this.board[i] === 0 && this.unClickableNodes.includes(nodes)) {
+              this.unClickableNodes = this.arrRemove(this.unClickableNodes, nodes)
+              betweenNodesCounter++
+              if (betweenNodesCounter === (node2Grid.row - node1Grid.row - 1)) {
+                this.unClickableNodes = this.arrRemove(this.unClickableNodes, nodes)
+                betweenNodesCounter = 0
+              }
+            }
+          }
+        }
+      })
     }
   },
   watch: {
-    numOfCols: function () {
+    numOfCols: function (i) {
       this.singleBridgeList = []
       this.doubleBridgeList = []
     }
--- src/components/board.js
+++ src/components/board.js
@@ -32,33 +32,13 @@ export default function boardGenerator (numOfCols, numOfRows) {
     nodeList.push({ position: firstRandomNodePos, arity: firstRandomNodeArity, connections: 0, freeNode: true, nodeToNode: [] })
     board[firstRandomNodePos] = 1
   }
-  // Sets surrounding positions to not available / to -1
-  /*   function setSurPos (nodeLoc) {
-    const gridPos = getGridPos(nodeLoc)
-    // node is not first el in a row
-    if (gridPos.col !== 0) {
-      board[nodeLoc - 1] = -1
-    }
-    // node is not last el in a row
-    if (gridPos.col !== numOfCols - 1) {
-      board[nodeLoc + 1] = -1
-    }
-    // node is not in the top row
-    if (gridPos.row !== 0) {
-      board[nodeLoc - numOfCols] = -1
-    }
-    // node is not in the bottom row
-    if (gridPos.row !== numOfRows - 1) {
-      board[nodeLoc + numOfCols] = -1
-    }
-  } */
   // set the positions inbetween to nodes to -1
   function setInbetweenPos (node1, node2) {
     const nodes = [node1, node2]
     nodes.sort((a, b) => a - b)
     const node1GridPos = getGridPos(nodes[0])
     const node2GridPos = getGridPos(nodes[1])
-    // same row
+    // same rowthis.connNtN = nodesConn
     if (node1GridPos.row === node2GridPos.row) {
       for (let i = nodes[0] + 1; i < nodes[1]; i++) {
         board[i] = -1
@@ -153,7 +133,7 @@ export default function boardGenerator (numOfCols, numOfRows) {
     }
   }
   //  FOR DEBUG PURPOSE, sets nodetonode prop to childnodes
-  function setNodeToNode (parentNodePos, newNodePos) {
+  /* function setNodeToNode (parentNodePos, newNodePos) {
     nodeList.map(el => {
       if (el.position === parentNodePos) {
         el.nodeToNode.push(newNodePos)
@@ -161,7 +141,7 @@ export default function boardGenerator (numOfCols, numOfRows) {
         el.nodeToNode.push(parentNodePos)
       }
     })
-  }
+  } */
   // loop to setup all the nodes on the board with N = 2 * number of max nodes
   function setNodes () {
     for (let i = 0; i < (numBoardNodes * 2); i++) {
@@ -187,15 +167,14 @@ export default function boardGenerator (numOfCols, numOfRows) {
         setConnections(newNode, parentNode)
         board[newNode.position] = 1
         setInbetweenPos(parentNode.position, newNode.position)
-        // for debug purpose
-        setNodeToNode(parentNode.position, newNode.position)
+        // for debug purpose setNodeToNode(parentNode.position, newNode.position)
       }
       if (nodeList.length === numBoardNodes) break
     }
   }
   let counter = 0
   let finalNodeList = []
-  let finalBoard = []
+  // FOR DEBUG let finalBoard = []
   // try untill it reached the set number of nodes or after 500 tries
   while (nodeList.length !== numBoardNodes && counter < 50) {
     nodeList = []
@@ -205,14 +184,13 @@ export default function boardGenerator (numOfCols, numOfRows) {
     // save largest list in case the set number of board nodes aren't reached
     if (nodeList.length > finalNodeList.length) {
       finalNodeList = [...nodeList]
-      finalBoard = [...board]
+    // FOR DEBUG  finalBoard = [...board]
     }
   }
-  console.log(finalBoard)
-  console.log(finalNodeList)
+
   /* find the nodes a certain node is connected to */
   // list all nodes and sort
-  /* const nodeLocList = []
+  const nodeLocList = []
   finalNodeList.map(element => {
     nodeLocList.push(element.position)
   })
@@ -223,7 +201,7 @@ export default function boardGenerator (numOfCols, numOfRows) {
     const gridPos = getGridPos(nodeLoc)
     // check positions in row left of node
     for (let i = nodeLoc - 2; i >= gridPos.row * numOfCols; i--) {
-      if (nodeLocList.inc1udes(i)) {
+      if (nodeLocList.includes(i)) {
         crossPos.push(i)
         break
       }
@@ -254,6 +232,6 @@ export default function boardGenerator (numOfCols, numOfRows) {
   // set the list of nodes a node is connected to as a property
   finalNodeList.map(el => {
     el.nodeToNode = getNodeToNode(el.position)
-  }) */
+  })
   return finalNodeList
 }
